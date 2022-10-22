8a4e1e450f96138a22224d93e1586d2ff9e47c50
Chmura <jakub.chmura@aptiv.com>
2022-10-22 15:20:56 +0200

    No idea what im doing

--- Zadanie2/src/Drawing.java
+++ Zadanie2/src/Drawing.java
@@ -1,10 +1,15 @@
+import java.util.Arrays;
+
 public class Drawing implements SimpleDrawing {
     int size;
-    int[][] board = this.getPainting();
+    int[][] painting;
+    int fistCord;
+    int secondCord;
     static class GeometryImpl implements Geometry{
         int size;
         int firstCord;
         int secondCord;
+
         @Override
         public int getSize() {
             return this.size;
@@ -19,7 +24,7 @@ public class Drawing implements SimpleDrawing {
         }
     }
 
-    class SegmentImpl implements Segment{
+    static class SegmentImpl implements Segment{
         private int direction;
         private int length;
         private int color;
@@ -41,24 +46,56 @@ public class Drawing implements SimpleDrawing {
 
     // Drawing class implementation
     @Override
-    public void setCanvasGeometry(Geometry input) {
-        size = input.getSize();
+    public void setCanvasGeometry(Geometry input)
+    {
+        this.painting = new int[input.getSize()][input.getSize()];
     }
 
     @Override
     public void draw(Segment segment) {
+        switch (segment.getDirection()){
+            case 1:
+                // 1 means right, so we need to increment the first coord, and leave the second as it is;
+                int targetFirstCord = this.fistCord + segment.getLength();
+                for (int currentIndex = this.fistCord; currentIndex < targetFirstCord; currentIndex++){
+                    this.painting[currentIndex][secondCord] = segment.getColor();
+                }
 
+        }
     }
 
     @Override
     public int[][] getPainting() {
-        return new int[this.size][this.size];
+        return this.painting;
     }
 
     @Override
-    public void clear() {}
+    public void clear() {
+        Arrays.stream(this.painting).forEach(a -> Arrays.fill(a, 0));
+    }
 
     public static void main(String[] args) {
+        // Configuring board related variables
         GeometryImpl newBoard = new GeometryImpl();
+        newBoard.size = 3;
+        newBoard.firstCord = 0;
+        newBoard.secondCord = 0;
+
+        // Configuring segment object
+        SegmentImpl newSegment = new SegmentImpl();
+        newSegment.color = 1;
+        newSegment.length = 3;
+        newSegment.direction = 1;
+
+        Drawing drawing = new Drawing();
+        drawing.setCanvasGeometry(newBoard);
+        drawing.draw(newSegment);
+        for (int i = 0; i < newBoard.getSize(); i++){
+            System.out.println("i row " + i);
+            for (int j = 0; j < newBoard.getSize(); j++){
+                System.out.println(drawing.painting[j][i]);
+            }
+        }
+
     }
 }
