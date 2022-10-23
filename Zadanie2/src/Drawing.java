b29c0a87cb1bcebf396b5a62664eb603d969735c
Chmura <jakub.chmura@aptiv.com>
2022-10-23 23:10:33 +0200

    Fixing minor bugs and removing implementation

--- Zadanie2/src/Drawing.java
+++ Zadanie2/src/Drawing.java
@@ -1,68 +1,5 @@
+
 public class Drawing implements SimpleDrawing{
-    public static class GeometryImpl implements Geometry{
-        int size;
-        int initialFirstCord;
-        int initialSecondCord;
-        @Override
-        public int getSize() {
-            /*
-            Method has to return desired size of board. (user input)
-             */
-            return this.size;
-        }
-
-        @Override
-        public int getInitialFirstCoordinate() {
-            /*
-            Method has to return desired first coordinate of board. (user input)
-             */
-            return initialFirstCord;
-        }
-
-        @Override
-        public int getInitialSecondCoordinate() {
-            /*
-            Method has to return desired size of board. (user input)
-             */
-            return initialSecondCord;
-        }
-    }
-
-    public static class SegmentImpl implements Segment{
-        int direction;
-        int color;
-        int length;
-
-        @Override
-        public int getDirection() {
-            /*
-            Returns direction of a segment we want to draw
-            1 -> Right
-            2 -> Up
-            -1 -> Left
-            -2 -> Down
-             */
-            return this.direction;
-        }
-
-        @Override
-        public int getLength() {
-            /*
-            Returns amount of pixels we want to color, that is number of elements of the
-            array that need to be modified
-             */
-            return this.length;
-        }
-
-        @Override
-        public int getColor() {
-            /*
-            Returns which color we want to color our segment, that is what number we'll put
-            in our elements.
-             */
-            return this.color;
-        }
-    }
     int size; // size of the board is (size x size)
     int firstCord;  // x-axis
     int secondCord; // y-axis
@@ -77,7 +14,9 @@ public class Drawing implements SimpleDrawing{
         this.size = input.getSize();
         this.firstCord = input.getInitialFirstCoordinate();
         this.secondCord = input.getInitialSecondCoordinate();
-        this.painting = new int[input.getSize()][input.getSize()];
+        if (this.size > 0 ) {
+            this.painting = new int[input.getSize()][input.getSize()];
+        }
     }
 
     @Override
@@ -132,7 +71,7 @@ public class Drawing implements SimpleDrawing{
                 this.secondCord++;
             }
             default -> {
-                System.out.println("Wrong input!");
+                System.out.println("Wrong direction!");
             }
         }
     }
@@ -144,13 +83,17 @@ public class Drawing implements SimpleDrawing{
         Note that if the Geometry is not initialized, the function is supposed to
         return null.
          */
-        int[][] newPainting = new int[this.size][this.size];
-        for (int i =0; i < this.size; i++){
-            for (int j = 0; j < this.size; j++){
-                newPainting[this.size-j-1][i] = this.painting[i][j];
-            }
-        }
-        this.painting = newPainting;
+
+        /* Ten fragment kodu odwraca tablice o 90 stopni, przeciwnie do ruchu wskazowek zegara,
+        zeby bylo zgodne z tym co
+        * jest na obrazku, wydaje mi sie ze to nie jest konieczne wiec komentuje.*/
+//        int[][] newPainting = new int[this.size][this.size];
+//        for (int i =0; i < this.size; i++){
+//            for (int j = 0; j < this.size; j++){
+//                newPainting[this.size-j-1][i] = this.painting[i][j];
+//            }
+//        }
+//        this.painting = newPainting;
         return this.painting;
     }
 
@@ -177,17 +120,5 @@ public class Drawing implements SimpleDrawing{
             }
             System.out.println("");
         }
-    }
-
-    public static void main(String[] args) {
-        GeometryImpl geometry = new GeometryImpl();
-        geometry.size = 5;
-        geometry.initialFirstCord = 1;
-        geometry.initialSecondCord = 2;
-
-        Drawing board = new Drawing();
-        board.setCanvasGeometry(geometry);
-
-
     }
 }
