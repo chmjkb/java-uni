aa056156bb2bed07e360db745957005f39147e3e
chmjkb <jakubchmura1607@gmail.com>
2022-10-29 18:48:39 +0200

    Redoing the assignment

--- Zadanie3/src/Meeting.java
+++ Zadanie3/src/Meeting.java
@@ -1,51 +1,82 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Meeting implements MeetingInterface{
+    int roundNumber = 1;
+    Position2D currentMeetingPoint;
+    List<PawnPosition2D> allPawns = new ArrayList<>();  // Helps with keeping track of pawnIds
 
-    int boardSize = 10;
-    int[][] board = new int[boardSize][boardSize];
-    int[] currentMeetingPoint;  // X,Y
     @Override
     public void addPawns(List<PawnPosition> positions) {
-
+        /*
+        Method responsible for adding pawns to the board
+         */
+        for (PawnPosition currentPawn : positions) {
+            int id = currentPawn.pawnId();
+            int x = currentPawn.x();
+            int y = currentPawn.y();
+            allPawns.add(new PawnPosition2D(id, x ,y));
+        }
     }
-
     @Override
     public void addMeetingPoint(Position meetingPointPosition) {
-        currentMeetingPoint[0] = Position.x();
-        currentMeetingPoint[1] = Position.y();
+        /*
+        Method responsible for updating the meeting point based on given Position object
+         */
+        int x = meetingPointPosition.x();
+        int y = meetingPointPosition.y();
+        currentMeetingPoint = new Position2D(x, y);
     }
 
     @Override
     public void move() {
-
+        /*
+        TODO
+         */
     }
-
     @Override
     public Set<PawnPosition> getAllPawns() {
+        /*
+        TODO
+         */
         return null;
     }
 
-    public boolean topOccupied(int i, int j){
-        if (board[i+1][j] == 0){
-            return true;
-        } else {
-            return false;
+    public String getPawnListIndex(int pawnId) {
+        for (int i = 0; i < this.allPawns.size(); i++) {
+            PawnPosition2D currentPawn = this.allPawns.get(i);
+            if (currentPawn.pawnId() == pawnId) {
+                return Integer.toString(i);
+            }
         }
+        return "No pawn with " + pawnId + "id.";
+    }
+
+    public boolean isOccupied(int x, int y){
+        for (PawnPosition2D currentPawn: allPawns){
+            if (currentPawn.x() == x && currentPawn.y() == y){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public Set<PawnPosition> getNeighbours(int pawnId) {
+        Set<PawnPosition> neighbours = new HashSet<>();
+        String pawnListIndex = getPawnListIndex(pawnId);
+        PawnPosition2D pawnOfInterest = allPawns.get(Integer.parseInt(pawnListIndex));
+        int pawn_x = pawnOfInterest.x();
+        int pawn_y = pawnOfInterest.y();
 
-        for (int i = 0; i < boardSize; i++){
-            for (int j = 0; i < boardSize; j++){
-                if (board[i][j] == pawnId){
-                    if (topOccupied(i, j)){
-
-                    }
-                }
-            }
+        if (isOccupied(pawn_x + 1, pawn_y)){
+            neighbours.add(new PawnPosition2D());
         }
+    }
+
+    public static void main(String[] args) {
+        Meeting myMeeting = new Meeting();
     }
 }
