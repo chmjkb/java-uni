f9280aef596bfa22c7b3d36f4335bee20d5255fa
chmjkb <jakubchmura1607@gmail.com>
2022-12-09 19:26:34 +0100

    Implementing interface

---
+++ Zadanie6/src/Archivizer.java
@@ -0,0 +1,11 @@
+public class Archivizer implements ArchivizerInterface{
+    @Override
+    public int compress(String dir, String filename) {
+        return 0;
+    }
+
+    @Override
+    public void decompress(String filename, String dir) {
+
+    }
+}
