0c7a7245cbf6f14d290b873802a0e6c584b941cb
chmjkb <jakubchmura1607@gmail.com>
2022-12-18 14:32:57 +0100

    Implementing isReady method

--- Zadanie7/src/FastHistogram.java
+++ Zadanie7/src/FastHistogram.java
@@ -1,6 +1,9 @@
-import java.util.Map;
+import java.util.*;
+import java.util.Vector;
 
 public class FastHistogram implements Histogram{
+
+    // https://www.youtube.com/watch?v=HofOMf8RgjM
 
     public class HistogramThread extends Thread{
         Vector inputVector;
@@ -14,20 +17,32 @@ public class FastHistogram implements Histogram{
             }
         }
     }
-    
+
+    List<Thread> currentThreads = new ArrayList<>();
+    List<Integer> currentVector = new ArrayList<>();
+    Queue<Vector> vectorQueue = new ArrayDeque<>();
+
     @Override
     public void setup(int threads, int bins) {
 
     }
 
-    @Override
-    public void setVector(Vector vector) {
-
-    }
+//    @Override
+//    public void setVector(Vector vector) {
+//        /*
+//        If all threads are done, we initiate a new vector for processing
+//         */
+//        if (isReady()){
+//            currentVector.clear();
+//            for (int i = 0; i < vector.getSize(); i++){
+//                currentVector.add(vector.getValue(i));
+//            }
+//        }
+//    }
 
     @Override
     public boolean isReady() {
-        return false;
+        return !currentThreads.stream().allMatch(Thread::isAlive);
     }
 
     @Override
