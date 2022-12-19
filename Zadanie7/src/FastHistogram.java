065c6347c66f999da867afd017eb542f6047f4b7
chmjkb <jakubchmura1607@gmail.com>
2022-12-19 23:47:51 +0100

    Finished assignment

--- Zadanie7/src/FastHistogram.java
+++ Zadanie7/src/FastHistogram.java
@@ -1,54 +1,80 @@
-import java.util.*;
-import java.util.Vector;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-public class FastHistogram implements Histogram{
+public class FastHistogram implements Histogram {
+    private int threads;
+    private int bins;
+    private Vector vector;
+    private Map<Integer, Integer> histogram = new ConcurrentHashMap<>();
+    private ExecutorService executor;
+    private boolean ready;
 
-    // https://www.youtube.com/watch?v=HofOMf8RgjM
 
-    public class HistogramThread extends Thread{
-        Vector inputVector;
-        public HistogramThread(Vector inputVector){
-            this.inputVector = inputVector;
-        }
-        @Override
-        public void run(){
-            for (int i = 0; i < inputVector.getSize(); i++){
-                System.out.println(inputVector.getValue(i));
-            }
-        }
-    }
-
-    List<Thread> currentThreads = new ArrayList<>();
-    List<Integer> currentVector = new ArrayList<>();
-    Queue<Vector> vectorQueue = new ArrayDeque<>();
 
     @Override
     public void setup(int threads, int bins) {
-
+        this.bins = bins;
+        this.threads = threads;
     }
 
-//    @Override
-//    public void setVector(Vector vector) {
-//        /*
-//        If all threads are done, we initiate a new vector for processing
-//         */
-//        if (isReady()){
-//            currentVector.clear();
-//            for (int i = 0; i < vector.getSize(); i++){
-//                currentVector.add(vector.getValue(i));
-//            }
-//        }
-//    }
+    @Override
+    public void setVector(Vector vector) {
+        ready = false;
+        this.vector = vector;
+        this.executor = Executors.newFixedThreadPool(threads);
+        histogram.clear();
+        int chunkSize = vector.getSize() / threads;
+        for (int i = 0; i < threads; i++) {
+            int start = i * chunkSize;
+            int end = (i == threads - 1) ? vector.getSize() : (i + 1) * chunkSize;
+            executor.execute(new HistogramRunnable(vector, start, end, histogram));
+        }
+
+        ExecutorService readyExecutor = Executors.newSingleThreadExecutor();
+        readyExecutor.execute(() -> {
+            try {
+                executor.awaitTermination(1, TimeUnit.HOURS);
+                ready = true;
+            } catch (InterruptedException e) {
+                // handle exception
+            }
+        });
+        readyExecutor.shutdown();
+        executor.shutdown();
+    }
 
     @Override
     public boolean isReady() {
-        return !currentThreads.stream().allMatch(Thread::isAlive);
+        return ready;
     }
 
     @Override
     public Map<Integer, Integer> histogram() {
-        return null;
+        return histogram;
+    }
+}
+
+class HistogramRunnable implements Runnable {
+    private final Vector vector;
+    private final int start;
+    private final int end;
+    private final Map<Integer, Integer> histogram;
+
+    public HistogramRunnable(Vector vector, int start, int end, Map<Integer, Integer> histogram) {
+        this.vector = vector;
+        this.start = start;
+        this.end = end;
+        this.histogram = histogram;
     }
 
-
+    @Override
+    public void run() {
+        for (int i = start; i < end; i++) {
+            int value = vector.getValue(i);
+            histogram.compute(value, (k, v) -> v == null ? 1 : v + 1);
+        }
+    }
 }
